8088d066816eae91845210e9910ad66c756d887a
mmedberry <maxrmedberry@gmail.com>
2020-02-13 17:47:02 -0600

    create characters and sort arrays

--- src/Components/characterForm.tsx
+++ src/Components/characterForm.tsx
@@ -1,9 +1,10 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, ReactElement } from "react";
 import Grommet, { Form, FormField, Button, Box } from "grommet";
-import { Trash, UserAdd } from "grommet-icons";
-import { Character } from "../types";
+import { Trash, UserAdd, Edit } from "grommet-icons";
+import { Character, Affiliation } from "../types";
 interface ICharacterFormProps {
     handleDeleteClick: () => void;
+    handleSubmitCharacterClick: (data: Character) => void;
     id: number;
 }
 interface ICharacterFormState {
@@ -11,17 +12,36 @@ interface ICharacterFormState {
     initiative: number;
     dex: number;
     hp: number;
+    submitted: boolean;
 }
 export class CharacterForm extends React.Component<ICharacterFormProps, ICharacterFormState>{
     state: ICharacterFormState = {
         name: "",
         initiative: 0,
         dex: 0,
-        hp: 0
-
+        hp: 0,
+        submitted: false
     }
     handleOnSubmit = () => {
-        console.log(this.state);
+
+        let character: Character = {
+            name: this.state.name,
+            initiative: this.state.initiative,
+            dex: this.state.dex,
+            hp: this.state.hp,
+            id: this.props.id,
+            affiliation: Affiliation.Ally
+        };
+        this.setState({
+            submitted: true
+        })
+        this.props.handleSubmitCharacterClick(character);
+
+    }
+    handleEditClick = () => {
+        this.setState({
+            submitted: false
+        })
     }
     handleChange = (event: React.FormEvent<HTMLInputElement>) => {
         const eventName = event.currentTarget.name;
@@ -49,24 +69,47 @@ export class CharacterForm extends React.Component<ICharacterFormProps, ICharact
             })
         }
     }
+    handleIsSubmitted = (): ReactElement => {
+        if (this.state.submitted) {
+            return (
+                <>
+                    <Box border="all" margin="small" round={true} overflow="hidden" pad="small" alignSelf="start" width="medium">
+
+                        <h1>
+                            {"Character " + this.props.id + ": " + this.state.name}
+                        </h1>
+                        <Button label="Edit" icon={<Edit />} onClick={this.handleEditClick}/>
+                    </Box>
+                </>
+            );
+        } else {
+            return (
+
+                <Box border="all" margin="small" round={true} overflow="hidden" pad="small" alignSelf="start" width="medium">
+
+                    <Form name="form" onSubmit={this.handleOnSubmit}>
+                        <h1>
+                            {"Character " + this.props.id}
+                        </h1>
+                        <FormField type="text" value={this.state.name} name="name" label="Name" required={true} onChange={this.handleChange} />
+                        <FormField name="initiative" label="Initiative" required={true} onChange={this.handleChange} />
+                        <FormField name="dex" label="Dexterity Modifier" type="number" required={true} onChange={this.handleChange} />
+                        <FormField name="hp" label="HP" type="number" required={true} onChange={this.handleChange} />
+                        <Button label="Submit" type="submit" icon={<UserAdd />} />
+                        <Button label="Delete" icon={<Trash />} onClick={this.props.handleDeleteClick} color="red" hoverIndicator="red" />
+
+                    </Form>
+                </Box>
+
+            );
+
+        }
+    }
     render() {
         return (
-            <Box border="all" margin="small" round={true} overflow="hidden" pad="small" alignSelf="start" width="medium">
-
-                <Form name="form" onSubmit={this.handleOnSubmit}>
-                    <h1>
-                        {"Character " + this.props.id}
-                    </h1>
-                    <FormField type="text" value={this.state.name} name="name" label="Name" required={true} onChange={this.handleChange} />
-                    <FormField name="initiative" label="Initiative" required={true} onChange={this.handleChange}/>
-                    <FormField name="dex" label="Dexterity Modifier" type="number" required={true} onChange={this.handleChange}/>
-                    <FormField name="hp" label="HP" type="number" required={true} onChange={this.handleChange}/>
-                    <Button label="Submit" type="submit" icon={<UserAdd/>}/>
-                    <Button label="Delete" icon={<Trash />} onClick={this.props.handleDeleteClick} color="red" hoverIndicator="red" />
-                    
-                </Form>
-            </Box>
-
-        );
-    }
+            <>
+                {this.handleIsSubmitted()}
+            </>
+        )
+    };
 }
--- src/Pages/combatManagerPage.tsx
+++ src/Pages/combatManagerPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grommet, Text, Form, FormField, Button, Box } from "grommet";
-import { Trash, Add } from "grommet-icons";
+import { Trash, Add, Play } from "grommet-icons";
 import { CharacterForm } from "../Components/characterForm";
 import { Character } from "../types";
 interface ICombatManagerProps {
@@ -9,17 +9,19 @@ interface ICombatManagerProps {
 interface ICombatManagerState {
     inCombat: boolean;
     characterCount: number;
-    characters: Character[]
+    characters: Character[];
+    currentTurn: number;
 }
 export class CombatManagerPage extends React.Component<ICombatManagerProps, ICombatManagerState> {
-    constructor(props: Readonly<ICombatManagerProps>){
+    constructor(props: Readonly<ICombatManagerProps>) {
         super(props);
-
+        this.handleSubmitCharacterClick = this.handleSubmitCharacterClick.bind(this);
     }
     state: ICombatManagerState = {
         inCombat: false,
-        characterCount: 1,
-        characters: []
+        characterCount: 0,
+        characters: [],
+        currentTurn: 0
     };
     handleDeleteClick = () => {
         let decrement = this.state.characterCount - 1;
@@ -33,25 +35,81 @@ export class CombatManagerPage extends React.Component<ICombatManagerProps, ICom
             characterCount: increment
         })
     }
-    handleSubmitCharacterClick=(data: any)=>{
+    handleSubmitCharacterClick = (data: Character) => {
         let tempArray = this.state.characters;
-        tempArray.push(new Character(data));
+        let addedElement = false;
+        for (let index = 0; index < tempArray.length; index++) {
+            const element = tempArray[index];
+            if (data.id === element.id) {
+                tempArray[index] = data;
+                addedElement = true;
+            }
+        }
+        if (!addedElement) {
+            tempArray.push(data);
+        }
         this.setState({
             characters: tempArray
         })
+        console.log(this.state.characters);
+    }
+    handleIncreaseTurn = () => {
+        let newTurn = this.state.currentTurn + 1;
+        if (newTurn >= this.state.characters.length) {
+            newTurn = 0;
+        }
+        this.setState({
+            currentTurn: newTurn
+        })
+    }
+    handleBeginCombatClick = () => {
+        let tempArray = this.state.characters.sort((a, b) => {
+            if (a.initiative === b.initiative) {
+                if (a.dex < b.dex) {
+                    return 1;
+                }
+                else {
+
+                    return -1;
+                }
+            } else if (a.initiative < b.initiative) {
+                return 1;
+            } else {
+                return -1
+            }
+        }
+
+        );
+        this.setState({
+            characters: tempArray, inCombat: true
+        });
+        console.log(this.state.characters);
     }
     render() {
         const forms = [];
-        for (let i = 1; i <= this.state.characterCount; i++) {
-            forms.push(<CharacterForm key={i} id={i} handleDeleteClick={this.handleDeleteClick} />)
+        if (this.state.inCombat) {
+            for (let i = 0; i <= this.state.characterCount; i++) {
+                forms.push(<Box key={i} border="all" margin="small" round={true} overflow="hidden" pad="small" alignSelf="start" width={this.state.currentTurn === i ? "medium" : "small"}>
+                    <Text>
+                        {this.state.characters[i].name}
+                    </Text>
+                    <Button label="Next"
+                        onClick={this.handleIncreaseTurn}></Button>
+                </Box>)
+            }
+        }
+        else {
+            for (let i = 0; i <= this.state.characterCount; i++) {
+                forms.push(<CharacterForm key={i} id={i} handleDeleteClick={this.handleDeleteClick} handleSubmitCharacterClick={this.handleSubmitCharacterClick} />)
+            }
         }
         return (
             <>
                 <Grommet >
-                    
                     {forms}
                 </Grommet>
-                <Button icon={<Add />} label="Add Character" onClick={this.handleAddCharacterClick} />
+                <Button icon={<Add />} label="Add Character" onClick={this.handleAddCharacterClick} disabled={this.state.inCombat} /><br /><br />
+                <Button icon={<Play />} label="Begin Combat" onClick={this.handleBeginCombatClick} disabled={this.state.inCombat} />
             </>
         );
     }
